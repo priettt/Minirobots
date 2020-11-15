db0c0f4dcf7854ecb2802ec873e3ada263aa9bb1
Francisco <priettt@gmail.com>
2020-11-15 16:32:27 -0300

    Basic CameraX implementation

--- app/src/main/java/com/example/minirobots/MainActivity.kt
+++ app/src/main/java/com/example/minirobots/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.minirobots
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.widget.FrameLayout
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import java.io.File
 
+/**
+ * Main entry point into our app. This app follows the single-activity pattern, and all
+ * functionality is implemented in the form of fragments.
+ */
 class MainActivity : AppCompatActivity() {
+    private lateinit var container: FrameLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        container = findViewById(R.id.fragment_container)
     }
 }
