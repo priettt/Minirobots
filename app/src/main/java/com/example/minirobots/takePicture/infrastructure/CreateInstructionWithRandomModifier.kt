5941b008070e90d9cd6f6fba46530fc8d8bda805
Francisco Prieto <fprieto@paypal.com>
2021-10-16 01:50:02 -0300

    Added tests for CreateInstructionWithRandomModifier.kt

--- app/src/main/java/com/example/minirobots/takePicture/infrastructure/CreateInstructionWithRandomModifier.kt
+++ app/src/main/java/com/example/minirobots/takePicture/infrastructure/CreateInstructionWithRandomModifier.kt
@@ -8,22 +8,10 @@ import javax.inject.Inject
 
 class CreateInstructionWithRandomModifier @Inject constructor() {
     operator fun invoke(action: Action): Instruction = when (action) {
-        AVANZAR -> Instruction(AVANZAR, NUMERO_AL_AZAR)
-        BAJAR_LAPIZ -> Instruction(BAJAR_LAPIZ, null)
-        FUNCION -> Instruction(FUNCION, null)
-        FUNCION_COMIENZO -> Instruction(FUNCION_COMIENZO, null)
-        FUNCION_FIN -> Instruction(FUNCION_FIN, null)
-        GIRAR_DERECHA -> Instruction(GIRAR_DERECHA, ANGULO_AL_AZAR)
-        GIRAR_IZQUIERDA -> Instruction(GIRAR_IZQUIERDA, ANGULO_AL_AZAR)
+        AVANZAR, RETROCEDER -> Instruction(action, NUMERO_AL_AZAR)
+        GIRAR_DERECHA, GIRAR_IZQUIERDA -> Instruction(action, ANGULO_AL_AZAR)
         LEDS -> Instruction(LEDS, COLOR_AL_AZAR)
-        LEVANTAR_LAPIZ -> Instruction(LEVANTAR_LAPIZ, null)
-        PROGRAMA_COMIENZO -> Instruction(PROGRAMA_COMIENZO, null)
-        PROGRAMA_FIN -> Instruction(PROGRAMA_FIN, null)
-        REPETIR_COMIENZO -> Instruction(REPETIR_COMIENZO, null)
-        REPETIR_FIN -> Instruction(REPETIR_FIN, null)
-        RETROCEDER -> Instruction(RETROCEDER, NUMERO_AL_AZAR)
-        TOCAR_CORCHEA -> Instruction(TOCAR_CORCHEA, NOTA_AL_AZAR)
-        TOCAR_MELODIA -> Instruction(TOCAR_MELODIA, NOTA_AL_AZAR)
-        TOCAR_NEGRA -> Instruction(TOCAR_NEGRA, NOTA_AL_AZAR)
+        TOCAR_CORCHEA, TOCAR_MELODIA, TOCAR_NEGRA -> Instruction(action, NOTA_AL_AZAR)
+        else -> Instruction(action, null)
     }
 }
---
+++ app/src/test/java/com/example/minirobots/takePicture/infrastructure/CreateInstructionWithRandomModifierTest.kt
@@ -0,0 +1,90 @@
+package com.example.minirobots.takePicture.infrastructure
+
+import com.example.minirobots.Action
+import com.example.minirobots.Instruction
+import com.example.minirobots.Modifier
+import org.junit.Assert.assertEquals
+
+import org.junit.Test
+
+class CreateInstructionWithRandomModifierTest {
+
+    val createInstructionWithRandomModifier = CreateInstructionWithRandomModifier()
+
+    private val movementAction = Action.RETROCEDER
+    private val turnAction = Action.GIRAR_DERECHA
+    private val ledsAction = Action.LEDS
+    private val musicAction = Action.TOCAR_NEGRA
+    private val nullModifierAction = Action.BAJAR_LAPIZ
+
+    @Test
+    fun `movement action`() {
+        val result = whenInvokedWithMovementAction()
+        shouldReturnActionWithRandomNumber(result)
+    }
+
+    @Test
+    fun `turn action`() {
+        val result = whenInvokedWithTurnAction()
+        shouldReturnActionWithRandomAngle(result)
+    }
+
+    @Test
+    fun `leds action`() {
+        val result = whenInvokedWithLedsAction()
+        shouldReturnActionWithRandomColor(result)
+    }
+
+    @Test
+    fun `music action`() {
+        val result = whenInvokedWithMusicAction()
+        shouldReturnActionWithRandomNote(result)
+    }
+
+    @Test
+    fun `null modifier action`() {
+        val result = whenInvokedWithNullModifierAction()
+        shouldReturnActionWithNullModifier(result)
+    }
+
+    private fun whenInvokedWithMovementAction(): Instruction {
+        return createInstructionWithRandomModifier(movementAction)
+    }
+
+    private fun whenInvokedWithTurnAction(): Instruction {
+        return createInstructionWithRandomModifier(turnAction)
+    }
+
+    private fun whenInvokedWithLedsAction(): Instruction {
+        return createInstructionWithRandomModifier(ledsAction)
+    }
+
+    private fun whenInvokedWithMusicAction(): Instruction {
+        return createInstructionWithRandomModifier(musicAction)
+    }
+
+    private fun whenInvokedWithNullModifierAction(): Instruction {
+        return createInstructionWithRandomModifier(nullModifierAction)
+    }
+
+    private fun shouldReturnActionWithRandomNumber(result: Instruction) {
+        assertEquals(Instruction(movementAction, Modifier.NUMERO_AL_AZAR), result)
+    }
+
+    private fun shouldReturnActionWithRandomAngle(result: Instruction) {
+        assertEquals(Instruction(turnAction, Modifier.ANGULO_AL_AZAR), result)
+    }
+
+    private fun shouldReturnActionWithRandomColor(result: Instruction) {
+        assertEquals(Instruction(ledsAction, Modifier.COLOR_AL_AZAR), result)
+    }
+
+    private fun shouldReturnActionWithRandomNote(result: Instruction) {
+        assertEquals(Instruction(musicAction, Modifier.NOTA_AL_AZAR), result)
+    }
+
+    private fun shouldReturnActionWithNullModifier(result: Instruction) {
+        assertEquals(Instruction(nullModifierAction, null), result)
+    }
+
+}
